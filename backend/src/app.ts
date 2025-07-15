1642aa462425a955bb1c8e4a7cbae9aa129177c7
Rafiul <rafiul13062001@gmail.com>
2025-07-16 01:00:14 +0600

    Successfully connected frontend with backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import poll_router from "./routes/poll_route";
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 //Routes
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,63 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type OptionModel = {
+  id: string
+  text: string
+  votes: number
+}
+
+type Poll = {
+  id: string
+  question: string
+  options: OptionModel[]
+  createdAt: Date
+  updatedAt: Date
+}
 
 const App = () => {
-  return <div></div>;
+
+  const [pollQuestions, setPollQuestions] = useState<Poll[]>([])
+
+  const getAllPolls = async (): Promise<void> => {
+    const response = await axios.get('http://localhost:3000/api/poll/get_all_polls')
+    console.log(response.data)
+    setPollQuestions(response.data)
+  }
+
+  useEffect(() => {
+    getAllPolls()
+  }, [])
+
+  return (
+    <div>
+      <h1>Poll App</h1>
+      <form action="" method="post">
+        {
+          pollQuestions.map((poll: Poll, index: number) => {
+            return (
+              <div key={index}>
+                <h2>{poll.question}</h2>
+                {
+                  poll.options.map((option: OptionModel, index: number) => {
+                    return (
+                      <div key={index}>
+                        <label key={index}>
+                          <input type="radio" name="option" value={option.id} />
+                          <span>{option.text}</span>
+                        </label>
+                        <br />
+                      </div>
+                    )
+                  })
+                }
+                <button type="submit">Submit</button>
+              </div>
+            )
+          })
+        }
+      </form>
+    </div>
+  );
 };
 export default App;
