3666ad09f01b2834a3be5b916117925b546ce278
Rafiul <rafiul13062001@gmail.com>
2025-07-17 21:34:29 +0600

    Updated Config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -11,9 +11,9 @@ interface Config {
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
-  mongodbUrl: process.env.MONGODB_URL || "",
-  databaseName: process.env.DATABASE_NAME || "",
-  collectionName: process.env.COLLECTION_NAME || "",
+  mongodbUrl: process.env.MONGODB_URL || "mongodb://localhost:27017/",
+  databaseName: process.env.DATABASE_NAME || "poll",
+  collectionName: process.env.COLLECTION_NAME || "question",
 };
 
 export default config;
